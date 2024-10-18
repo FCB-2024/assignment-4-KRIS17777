27564a4ce4386e27b778d3df8dc8a003964ac14c
kristinkahulic <kristinka.hulic01@estudiant.upf.edu>
2024-10-18 22:44:15 +0200

    Added src

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,9 +1,32 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+import sys
+def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+
+        i = 1
+        r = 0
+        while i <= x:
+                if x%i == 0:
+                        r = r + 1
+                i = i + 1
+        l = x - 1
+        s = 0
+        while l >= 1 and s < r:
+                a = 1
+                s = 0
+                while a <= l:
+                        if l%a == 0:
+                                s = s + 1
+                        a = a + 1
+                l = l - 1
+        if s >= r:
+                res = "not anti-prime"
+        else:
+                res = "prime"
+        return(res)
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
@@ -11,7 +34,7 @@ def main() :
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
@@ -21,4 +44,5 @@ if __name__ == "__main__" :
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	x = int(sys.argv[1])	
+	print(main(x))
